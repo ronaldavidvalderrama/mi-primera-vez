0e564deccfe4a4812b3cbcae1e9637306140aafe
Ronal David Valderrama Gomez <189985514+ronaldavidvalderrama@users.noreply.github.com>
2024-12-03 07:54:53 -0500

    Temas vistos , varibales, tipos de datos listas, entrada y saolida de datos

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello world")
+#RESUMEN:
+#VARIABLES
+#TIPOS DE DATOS 
+#-string cadena de caracter "atr"
+#-char = caracter "str"
+# - number = enteros "int" decimal  "floot" 
+# bolean = datos logicos  "bool" 
+
+
+
+# ASIGANCION DE VARIABLES  
+Name = "ronal valderrama"
+age = 20
+height = 1.70
+trainer = False
+
+# CONJUNTO DE DATOS
+#- array = conjunto de datos de una dimencion "list"
+hobbies = list()
+hobbies = ["jugar", "dormir","comer", "boxear"] 
+# CAMBIAR UN DATO 
+#hobbies[2] = "despertarse"
+
+#INGRESAR UN DATO 
+#dato = input("ingrese otro pasatiempo: ")
+#hobbies.append(dato)
+#print(hobbies)ç
+
+#ELIMINAR UN DATO 
+hobbies.pop(2)
+print(hobbies)
